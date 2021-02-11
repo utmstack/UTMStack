749baa057ff917d6488484e0be2f341bc404f042
Osmany Montero <osmontero@icloud.com>
2021-02-11 01:32:54 +0000

    will be remoed

--- utils/utils_linux.go
+++ utils/utils_linux.go
@@ -4,17 +4,17 @@ package utils
 
 func installDocker(mode string) {
 	aptGet(mode, "update")
-	aptGet(mode, "install", "apt-transport-https", "ca-certificates", "curl", "gnupg-agent", "software-properties-common")
+	aptGet(mode, "install", "-y", "apt-transport-https", "ca-certificates", "curl", "gnupg-agent", "software-properties-common")
 
 	env := []string{"DEBIAN_FRONTEND=noninteractive"}
 	runEnvCmd(mode, env, "sh", "-c", "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -")
 	runEnvCmd(mode, env, "sh", "-c", `add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"`)
 
 	aptGet(mode, "update")
-	aptGet(mode, "install", "docker-ce", "docker-ce-cli", "containerd.io")
+	aptGet(mode, "install", "-y", "docker-ce", "docker-ce-cli", "containerd.io")
 }
 
-func aptGet(mode, command string, arg ...string) error {
+func aptGet(mode, arg ...string) error {
 	env := []string{"DEBIAN_FRONTEND=noninteractive"}
-	return runEnvCmd(mode, env, "apt-get", append([]string{"--yes"}, arg...)...)
+	return runEnvCmd(mode, env, "apt", arg...)
 }
