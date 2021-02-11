d6be267494a27578d6d046ab67670d1dee982bae
Osmany Montero <osmontero@icloud.com>
2021-02-10 20:39:57 -0500

    Install docker without the apt function

--- utils/utils_linux.go
+++ utils/utils_linux.go
@@ -3,18 +3,13 @@
 package utils
 
 func installDocker(mode string) {
-	aptGet(mode, "update")
-	aptGet(mode, "install", "-y", "apt-transport-https", "ca-certificates", "curl", "gnupg-agent", "software-properties-common")
-
 	env := []string{"DEBIAN_FRONTEND=noninteractive"}
+	runEnvCmd(mode, env, "apt","update")
+	runEnvCmd(mode, env, "apt", "install", "-y", "apt-transport-https", "ca-certificates", "curl", "gnupg-agent", "software-properties-common")
+
 	runEnvCmd(mode, env, "sh", "-c", "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -")
 	runEnvCmd(mode, env, "sh", "-c", `add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"`)
 
-	aptGet(mode, "update")
-	aptGet(mode, "install", "-y", "docker-ce", "docker-ce-cli", "containerd.io")
-}
-
-func aptGet(mode, arg ...string) error {
-	env := []string{"DEBIAN_FRONTEND=noninteractive"}
-	return runEnvCmd(mode, env, "apt", arg...)
+	runEnvCmd(mode, env,"apt","update")
+	runEnvCmd(mode, env,"apt","install", "-y", "docker-ce", "docker-ce-cli", "containerd.io")
 }
