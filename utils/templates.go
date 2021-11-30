341d5a34ab3cb5a53fab4e546524e2caf9c4e5ee
Osmany Montero <osmontero@icloud.com>
2021-11-30 21:57:02 +0300

    renaming environment variables for panel

--- utils/templates.go
+++ utils/templates.go
@@ -278,21 +278,13 @@ services:
     environment:
       - SERVER_NAME
       - LITE
-      - POSTGRESQL_USER=postgres
-      - POSTGRESQL_PASSWORD=${DB_PASS}
-      - POSTGRESQL_HOST=${DB_HOST}
-      - POSTGRESQL_PORT=5432
-      - POSTGRESQL_DATABASE=utmstack
+      - DB_USER=postgres
+      - DB_PASS
+      - DB_HOST
+      - DB_PORT=5432
+      - DB_NAME=utmstack
       - ELASTICSEARCH_HOST=${DB_HOST}
       - ELASTICSEARCH_PORT=9200
-      - OPENVAS_HOST=openvas
-      - OPENVAS_PORT=9390
-      - OPENVAS_USER=admin
-      - OPENVAS_PASSWORD=${DB_PASS}
-      - OPENVAS_PG_PORT=5432
-      - OPENVAS_PG_DATABASE=gvmd
-      - OPENVAS_PG_USER=gvm
-      - OPENVAS_PG_PASSWORD=${DB_PASS}
     deploy:
       resources:
         limits:
