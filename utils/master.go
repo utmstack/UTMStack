05da6e3ba6f23b6776985a47cc5a4bad9820b57c
Osmany Montero <osmontero@icloud.com>
2022-01-28 21:13:25 +0300

    fixing issue with Docker Swarm advertise address

--- utils/master.go
+++ utils/master.go
@@ -95,7 +95,7 @@ func InstallMaster(mode, datadir, pass, tag string, lite bool) error {
 		return err
 	}
 
-	if err := InitDocker(mode, masterTemplate, env, true, tag, lite); err != nil {
+	if err := InitDocker(mode, masterTemplate, env, true, tag, lite, mainIP); err != nil {
 		return err
 	}
 
