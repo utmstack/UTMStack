2e6792c7fe8bd15580862243ef2614fbcd2af249
Osmany Montero <osmontero@icloud.com>
2022-03-10 16:03:04 +0200

    removing uninstall scanner

--- utils/uninstall.go
+++ utils/uninstall.go
@@ -13,23 +13,6 @@ func Uninstall(mode string) error {
 	// sleep while docker is removing the containers
 	time.Sleep(2 * time.Minute)
 
-	// uninstall scanner
-	if err := RunCmd(mode, "systemctl", "stop", "utm_scanner"); err != nil {
-		return err
-	}
-
-	if err := RunCmd(mode, "rm", "/etc/systemd/system/utm_scanner.service"); err != nil {
-		return err
-	}
-
-	if err := RunCmd(mode, "systemctl", "daemon-reload"); err != nil {
-		return err
-	}
-
-	if err := RunCmd(mode, "rm", "-R", "/opt/scanner/"); err != nil {
-		return err
-	}
-
 	// uninstall suricata
 	if err := RunCmd(mode, "apt", "remove", "-y", "--purge", "suricata"); err != nil {
 		return err
