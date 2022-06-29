b48def5063fda6875e1b3ddf2bc9fa6e4369ce47
Osmany Montero <osmontero@icloud.com>
2022-06-30 00:37:14 +0300

    run-parts does not support dots!

--- utils/updates.go
+++ utils/updates.go
@@ -1,11 +1,11 @@
 package utils
 
 func ConfigureUpdater(mode string) error {
-	if err := RunCmd(mode, "wget", "http://github.com/AtlasInsideCorp/UTMStackInstaller/releases/latest/download/updater.sh", "-O", "/etc/cron.hourly/updater.sh"); err != nil {
+	if err := RunCmd(mode, "wget", "http://github.com/AtlasInsideCorp/UTMStackInstaller/releases/latest/download/updater.sh", "-O", "/etc/cron.hourly/updater"); err != nil {
 		return err
 	}
 
-	if err := RunCmd(mode, "chmod", "755", "/etc/cron.hourly/updater.sh"); err != nil {
+	if err := RunCmd(mode, "chmod", "755", "/etc/cron.hourly/updater"); err != nil {
 		return err
 	}
 
