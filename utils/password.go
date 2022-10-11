dd7c6a2f9c5ad2df1933246a18d10073f530901e
Osmany Montero <osmontero@icloud.com>
2022-10-11 10:52:52 +0300

    linter auto-format

--- utils/password.go
+++ utils/password.go
@@ -17,7 +17,7 @@ func GenerateSecret() string {
 		}
 		rand.Seed(time.Now().UTC().UnixNano())
 		slice := map[int]string{0: string(capitals[rand.Intn(len(capitals))]), 1: string(lowers[rand.Intn(len(lowers))]), 2: string(numbers[rand.Intn(len(numbers))]), 3: "."}
-		for _, v := range(slice){
+		for _, v := range slice {
 			s += v
 		}
 	}
