51e76e31017b328011dc2456085fca2a90d754cb
Kbayero <yorjaKbayero@gmail.com>
2023-10-24 16:50:14 +0300

    Fix input pipeline in google cloud and azure cloud integrations

--- mutate/cloud_integrations/azure_integration.py
+++ mutate/cloud_integrations/azure_integration.py
@@ -38,17 +38,19 @@ class AzureIntegration(Integration):
                 if azure_configs is not None:
                     config = get_event_hubs(azure_configs)
                     if config is not None:
-                        azure += "azure_event_hubs {\n" \
-                                 "   config_mode => \"advanced\"\n" \
-                                 "   id => \"$group\"\n" \
-                                 "   add_field => { \"[@metadata][dataSource]\" => \"$group\" }\n" \
-                                 "   type => \"azure\"\n" \
-                                 "   decorate_events => true\n" \
-                                 "   event_hubs => [\n" \
-                                 "      $event_hubs\n" \
-                                 "   ]\n" \
-                                 " }\n "
-                        return Template(azure).substitute(event_hubs=config, group=group)
+                        azure += """input{ 
+                                        azure_event_hubs {
+                                            config_mode => "advanced"
+                                            id => "{}"
+                                            add_field => { "[@metadata][dataSource]" => "{}" }
+                                            type => "azure"
+                                            decorate_events => true
+                                            event_hubs => [
+                                                {}
+                                            ]
+                                        }
+                                    } """.format(group, group, config)
+                        return azure
                     else:
                         pass
                 else:
