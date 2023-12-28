249d5e5b0aac932363ae22c57b1494fb64c21dd8
manuel <manuel@quantfall.com>
2023-12-28 16:42:52 +0200

    Fix bug #169 Show a message describing that the alert has an incident already associated

--- frontend/src/app/data-management/alert-management/shared/components/alert-actions/alert-apply-incident/alert-apply-incident.component.ts
+++ frontend/src/app/data-management/alert-management/shared/components/alert-actions/alert-apply-incident/alert-apply-incident.component.ts
@@ -41,28 +41,45 @@ export class AlertApplyIncidentComponent implements OnInit {
     }
   }
 
-
-  createIncident() {
-    const modal = this.modalService.open(CreateIncidentComponent, {size: 'lg', backdrop: 'static', centered: true});
+  checkAlerts() {
     if (this.multiple && this.alerts && this.alerts.length > 0) {
-      modal.componentInstance.alerts = this.alerts;
+      const nonIncidentAlerts = this.alerts.filter(({ isIncident }) => !isIncident);
+      if (nonIncidentAlerts.length > 0) {
+        return nonIncidentAlerts;
+      } else {
+        this.utmToastService.showError(this.alerts && this.alerts.length > 1 ? 'Alerts associated with incident'
+            : 'Alert associated with incident',
+          this.alerts && this.alerts.length > 1 ? 'The selected alerts are associated with an incident.'
+            : 'The selected alert is associated with an incident.');
+        return [];
+      }
     } else {
-      modal.componentInstance.alerts = [this.alert];
+      return [this.alert];
     }
-    modal.componentInstance.incidentAdded.subscribe((incident) => {
-      this.markAsIncident.emit(incident.id.toString());
-    });
   }
 
   addToIncident() {
-    const modal = this.modalService.open(AddAlertToIncidentComponent, {size: 'lg', backdrop: 'static', centered: true});
-    if (this.multiple && this.alerts && this.alerts.length > 0) {
-      modal.componentInstance.alerts = this.alerts;
-    } else {
-      modal.componentInstance.alerts = [this.alert];
+    const alerts = this.checkAlerts();
+    if (alerts.length > 0) {
+      const modal = this.modalService.open(AddAlertToIncidentComponent, {size: 'lg', backdrop: 'static', centered: true});
+      modal.componentInstance.alerts = alerts;
+
+      modal.componentInstance.incidentAdded.subscribe((incident) => {
+        this.markAsIncident.emit(incident.id.toString());
+      });
     }
-    modal.componentInstance.incidentAdded.subscribe((incident) => {
-      this.markAsIncident.emit(incident.id.toString());
-    });
   }
+
+  createIncident() {
+    const alerts = this.checkAlerts();
+    if (alerts.length > 0) {
+      const modal = this.modalService.open(CreateIncidentComponent, {size: 'lg', backdrop: 'static', centered: true});
+      modal.componentInstance.alerts = alerts;
+
+      modal.componentInstance.incidentAdded.subscribe((incident) => {
+        this.markAsIncident.emit(incident.id.toString());
+      });
+    }
+  }
+
 }
