60ab9c75c1cdcb0c47958f6ddd773a7d32e61767
Yorjander Hernandez Vergara <99102374+Kbayero@users.noreply.github.com>
2024-01-22 22:12:44 +0200

    Automatic deploy for agent versions. (#343)
    * Automatic deploy for agent versions.
    
    * Create a new agent installer from the UTMStack version

--- .github/scripts/agent-deploy.py
+++ .github/scripts/agent-deploy.py
@@ -1,36 +1,77 @@
 import argparse
 import json
 import os
+import requests
 from google.cloud import storage
+import yaml
 
-def main(update_version, file_type, binary):
+def main(environment):
 	gcp_key = json.loads(os.environ.get("GCP_KEY"))
 	storage_client = storage.Client.from_service_account_info(gcp_key)
 
 	bucket_name = "utmstack-updates"
 	bucket = storage_client.bucket(bucket_name)
-    
-	if file_type == "installer":
-		endp = "agent_updates/release/installer/"
-	else :
-		endp = "agent_updates/release/" + file_type + "_service/"
 
-    # Upload files
-	windows_blob = bucket.blob(endp + update_version + "/" + binary +  ".exe")
-	windows_blob.upload_from_filename(os.path.join(os.environ["GITHUB_WORKSPACE"], "agent", file_type, (binary+".exe")))
+	# Read utmstack version from version.yml
+	with open(os.path.join(os.environ["GITHUB_WORKSPACE"], "version.yml"), "r") as f:
+		local_master_version = yaml.safe_load(f)['version']
 
-	linux_blob = bucket.blob(endp + update_version + "/" + binary)
-	linux_blob.upload_from_filename(os.path.join(os.environ["GITHUB_WORKSPACE"],"agent", file_type, binary))
+	# Read agent services version from versions.json
+	with open(os.path.join(os.environ["GITHUB_WORKSPACE"], "agent", "versions.json"), "r") as f:
+		local_agent_versions = json.load(f)
 
-    # Update version.json
-	version_blob = bucket.blob("agent_updates/release/versions.json")
-	version_blob.upload_from_filename(os.path.join(os.environ["GITHUB_WORKSPACE"],"agent", "versions.json"))
+	# Download versions.json from URL
+	endp = "agent_updates/{}".format(environment)
+	response = requests.get(endp + "/versions.json")
+	remote_versions = response.json()['versions']
+
+	# Find the object with matching master_version
+	version_found = False
+	for obj in remote_versions:
+		if obj['master_version'] == local_master_version:
+			obj['agent_version'] = local_agent_versions["agent_version"]
+			obj['updater_version'] = local_agent_versions["updater_version"]
+			obj['redline_version'] = local_agent_versions["redline_version"]
+			version_found = True
+			break
+
+	# If no matching object found, create a new one
+	if version_found == False:
+		version_obj = {
+			'master_version': local_master_version,
+			'agent_version': local_agent_versions["agent_version"],
+			'updater_version': local_agent_versions["updater_version"],
+			'redline_version': local_agent_versions["redline_version"],
+		}
+		remote_versions.append(version_obj)
+		
+	# Update version.json
+	version_blob = bucket.blob(endp + "/versions.json")
+	version_blob.upload_from_string(json.dumps({'versions': remote_versions}, indent=4))
+	
+	# Create agent blobs
+	agent_windows_blob = bucket.blob(endp + "/agent_service/v" + local_agent_versions["agent_version"] + "/utmstack_agent_service.exe")
+	agent_linux_blob = bucket.blob(endp + "/agent_service/v" + local_agent_versions["agent_version"] + "/utmstack_agent_service")
+	updater_windows_blob = bucket.blob(endp + "/updater_service/v" + local_agent_versions["updater_version"] + "/utmstack_updater_service.exe")
+	updater_linux_blob = bucket.blob(endp + "/updater_service/v" + local_agent_versions["updater_version"] + "/utmstack_updater_service")
+	redline_windows_blob = bucket.blob(endp + "/redline_service/v" + local_agent_versions["redline_version"] + "/utmstack_redline_service.exe")
+	redline_linux_blob = bucket.blob(endp + "/redline_service/v" + local_agent_versions["redline_version"] + "/utmstack_redline_service")
+	installer_windows_blob = bucket.blob(endp + "/installer/v" + local_master_version + "/utmstack_agent_installer.exe")
+	installer_linux_blob = bucket.blob(endp + "/installer/v" + local_master_version + "/utmstack_agent_installer")
+
+	# Upload agent services
+	agent_windows_blob.upload_from_filename(os.path.join(os.environ["GITHUB_WORKSPACE"], "agent", "agent_service", "utmstack_agent_service.exe"))
+	agent_linux_blob.upload_from_filename(os.path.join(os.environ["GITHUB_WORKSPACE"], "agent", "agent_service", "utmstack_agent_service"))
+	updater_windows_blob.upload_from_filename(os.path.join(os.environ["GITHUB_WORKSPACE"], "agent", "updater_service", "utmstack_updater_service.exe"))
+	updater_linux_blob.upload_from_filename(os.path.join(os.environ["GITHUB_WORKSPACE"], "agent", "updater_service", "utmstack_updater_service"))
+	redline_windows_blob.upload_from_filename(os.path.join(os.environ["GITHUB_WORKSPACE"], "agent", "redline_service", "utmstack_redline_service.exe"))
+	redline_linux_blob.upload_from_filename(os.path.join(os.environ["GITHUB_WORKSPACE"], "agent", "redline_service", "utmstack_redline_service"))
+	installer_windows_blob.upload_from_filename(os.path.join(os.environ["GITHUB_WORKSPACE"], "agent", "installer", "utmstack_agent_installer.exe"))
+	installer_linux_blob.upload_from_filename(os.path.join(os.environ["GITHUB_WORKSPACE"], "agent", "installer", "utmstack_agent_installer"))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Update UTMStack agents in Google Cloud Storage")
-    parser.add_argument("update_version", type=str, help="Update version string (v1.0.1)")
-    parser.add_argument("file_type", type=str, help="File type string(agent, installer, redline, updater)")
-    parser.add_argument("binary", type=str, help="Binary string(utmstack_agent_installer)")
+    parser.add_argument("environment", type=str, help="Environment(dev, qa, rc, release)")
     
     args = parser.parse_args()
-    main(args.update_version, args.file_type, args.binary)
+    main(args.environment)
