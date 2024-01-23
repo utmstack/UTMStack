ffc34be194836834c1fe6e979bfe014d0c068667
Manuel Abascal <mjabascal10@gmail.com>
2024-01-23 13:48:28 -0600

    Bugfix/10.2/333 incorrect linux install command (#361)
    * Added a dynamically generated version tag for agent installations.
    
    * Added a dynamically generated version tag for agent installations.
    
    * Fixed Incorrect Linux Agent install command (#333)
    
    * Fixed Command copy is confusing (#334)

--- frontend/src/app/app-module/module-integration/module-integration.component.ts
+++ frontend/src/app/app-module/module-integration/module-integration.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {UtmModulesEnum} from '../shared/enum/utm-module.enum';
 import {UtmModuleType} from '../shared/type/utm-module.type';
+import {CheckForUpdatesService} from "../../shared/services/updates/check-for-updates.service";
 
 @Component({
   selector: 'app-module-integration',
@@ -11,11 +12,19 @@ export class ModuleIntegrationComponent implements OnInit {
   @Input() module: UtmModuleType;
   @Input() serverId: number;
   moduleEnum = UtmModulesEnum;
+  currentVersion: string;
 
-  constructor() {
+  constructor(private checkForUpdatesService: CheckForUpdatesService) {
   }
 
   ngOnInit() {
+    this.getVersionInfo();
+  }
+
+  getVersionInfo() {
+    this.checkForUpdatesService.getVersion().subscribe(response => {
+      this.currentVersion = response.body.build.version;
+    });
   }
 
 }
