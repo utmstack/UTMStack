cd3982f8cd0ef440373640b557cbc42482fd4d78
Manuel Abascal <mjabascal10@gmail.com>
2024-01-26 17:24:36 +0200

    feature Improvement-for-the-create-schedule-report-compliance-component (#369)

--- frontend/src/app/shared/components/utm/filters/dashboard-filter-view/dashboard-filter-view.component.ts
+++ frontend/src/app/shared/components/utm/filters/dashboard-filter-view/dashboard-filter-view.component.ts
@@ -1,6 +1,11 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {DashboardFilterType} from '../../../../types/filter/dashboard-filter.type';
 
+
+export enum DashboardFilterLayout {
+  ScheduleReportCompliance,
+  Default
+}
 @Component({
   selector: 'app-dashboard-filter-view',
   templateUrl: './dashboard-filter-view.component.html',
@@ -9,8 +14,10 @@ import {DashboardFilterType} from '../../../../types/filter/dashboard-filter.typ
 export class DashboardFilterViewComponent implements OnInit {
   @Input() filters: DashboardFilterType[];
   @Input() building = false;
+  @Input() layout: DashboardFilterLayout = DashboardFilterLayout.Default;
   @Input() position: 'left' | 'right' = 'right';
   @Output() filterAction = new EventEmitter<{ action: 'edit' | 'delete', filter: DashboardFilterType }>();
+  DashboardFilterLayout = DashboardFilterLayout;
 
 
   constructor() {
@@ -28,6 +35,4 @@ export class DashboardFilterViewComponent implements OnInit {
   doAction(action: 'edit' | 'delete', filter: DashboardFilterType) {
     this.filterAction.emit({action, filter});
   }
-
-
 }
