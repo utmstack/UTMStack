db7a12e27966863106db3f56967aeb891e2f82b0
Yorjander Hernandez Vergara <yorjaKbayero@gmail.com>
2024-01-26 18:43:49 +0200

    Fix workflow for automatic agent deploy

--- agent/agent/main.go
+++ agent/agent/main.go
@@ -96,7 +96,7 @@ func main() {
 			msg := os.Args[2]
 			logp := logservice.GetLogProcessor()
 
-			// Read current config
+			// Read config
 			cnf, err := configuration.GetCurrentConfig()
 			if err != nil {
 				os.Exit(0)
