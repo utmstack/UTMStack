4802e36599c14e185a35a73289cc8e2354c64065
Osmany Montero <osmontero@icloud.com>
2024-02-12 22:05:26 +0200

    avoid stop processing routine because of a lock

--- office365/processor/sendData.go
+++ office365/processor/sendData.go
@@ -2,35 +2,33 @@ package processor
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
-	"github.com/quantfall/holmes"
+	"github.com/threatwinds/logger"
 	"github.com/utmstack/UTMStack/office365/configuration"
 	"github.com/utmstack/UTMStack/office365/utils"
 )
 
-func SendToCorrelation(data []TransformedLog, h *holmes.Logger) error {
-	h.Info("Uploading %d logs...", len(data))
+func SendToCorrelation(data []TransformedLog) *logger.Error {
+	utils.Logger.Info("uploading %d logs...", len(data))
 
 	for _, log := range data {
 		body, err := json.Marshal(log)
 		if err != nil {
-			h.Error("error encoding log to JSON: %v", err)
+			utils.Logger.ErrorF(http.StatusInternalServerError, "error encoding log to JSON: %v", err)
 			continue
 		}
 
-		resp, status, err := utils.DoReq[map[string]interface{}](configuration.CORRELATIONURL, body, http.MethodPost, map[string]string{})
-		if err != nil {
-			h.Error(fmt.Sprintf("Error sending log to correlation engine: %v", err))
+		_, status, e := utils.DoReq[map[string]interface{}](configuration.CORRELATIONURL, body, http.MethodPost, map[string]string{})
+		if e != nil {
 			continue
 		} else if status != http.StatusOK && status != http.StatusCreated {
-			h.Error(fmt.Sprintf("Error sending log to correlation engine with status code %d: %v", status, resp))
 			continue
 		}
 
-		h.Info("Log successfully sent to correlation engine")
+		utils.Logger.Info("log successfully sent to correlation engine")
 	}
-	h.Info("All logs sent to correlation")
+
+	utils.Logger.Info("all logs were sent to correlation")
 	return nil
 }
