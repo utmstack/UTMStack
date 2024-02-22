4bbab23ca0836c5d98428c98954cc3eff15597ed
Manuel Abascal <mjabascal10@gmail.com>
2024-02-22 19:06:59 +0200

    Feature Created SOC_AI resource (#460)

--- backend/src/main/java/com/park/utmstack/web/rest/soc_ai/UtmSocAiResource.java
+++ backend/src/main/java/com/park/utmstack/web/rest/soc_ai/UtmSocAiResource.java
@@ -6,11 +6,14 @@ import com.park.utmstack.service.soc_ai.SocAIService;
 import com.park.utmstack.web.rest.AccountResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/soc-ai")
@@ -28,17 +31,17 @@ public class UtmSocAiResource {
     }
 
     @PostMapping("/send-alerts")
-    public ResponseEntity<String> sendData(@RequestBody String[] alertsId) {
+    public ResponseEntity<Object> sendData(@RequestBody String[] alertsId) {
         final String ctx = CLASSNAME + ".sendAlertsIds";
         try {
             socAIService.sendData(alertsId);
-            return ResponseEntity.ok("Processing successful");
+            return ResponseEntity.ok().body(Map.of("status", "success", "message", "Processing successful"));
         } catch (Exception e) {
             String msg = ctx + ": " + e.getMessage();
             log.error(msg);
             applicationEventService.createEvent(msg, ApplicationEventType.ERROR);
 
-            return ResponseEntity.internalServerError().body(msg);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("status", "error", "message", msg));
         }
     }
 }
