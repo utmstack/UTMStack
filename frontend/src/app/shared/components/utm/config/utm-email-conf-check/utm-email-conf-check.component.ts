b33fb2a93199a05e1d9975f98b082a9bdbd67fd2
Manuel Abascal <mjabascal10@gmail.com>
2024-03-13 15:41:22 +0200

    Improvement Email settings hang for 15 minutes when checking if the configuration is wrong (#533)

--- frontend/src/app/shared/components/utm/config/utm-email-conf-check/utm-email-conf-check.component.ts
+++ frontend/src/app/shared/components/utm/config/utm-email-conf-check/utm-email-conf-check.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {AccountService} from '../../../../../core/auth/account.service';
 import {UtmToastService} from '../../../../alert/utm-toast.service';
 import {UtmConfigEmailCheckService} from '../../../../services/config/utm-config-email-check.service';
+import {SectionConfigParamType} from "../../../../types/configuration/section-config-param.type";
 
 @Component({
   selector: 'app-utm-email-conf-check',
@@ -10,6 +11,7 @@ import {UtmConfigEmailCheckService} from '../../../../services/config/utm-config
 })
 export class UtmEmailConfCheckComponent implements OnInit {
   @Input() validConfig: boolean;
+  @Input() emailConfig: SectionConfigParamType[] = [];
   checking: any;
   email: string;
 
@@ -22,15 +24,21 @@ export class UtmEmailConfCheckComponent implements OnInit {
   }
 
   testConfig() {
+    console.log(this.emailConfig);
     this.checking = true;
     this.accountService.identity().then(account => {
       this.utmConfigEmailCheckService.checkEmail().subscribe(() => {
         this.checking = false;
         this.utmToastService.showSuccessBottom('A test email has been sent to ' + account.email + '; please check your inbox.');
-      }, () => {
+      }, (error) => {
         this.checking = false;
-        this.utmToastService.showError('Error sending email',
-          'An error occurred while sending the test email, please contact with the support team');
+        if (error.status === 400) {
+          this.utmToastService.showError('Error sending email',
+            'An error occurred while sending the test email, please check configuration and try again');
+        } else {
+          this.utmToastService.showError('Error sending email',
+            'An error occurred while sending the test email, please contact with the support team');
+        }
       });
     });
   }
