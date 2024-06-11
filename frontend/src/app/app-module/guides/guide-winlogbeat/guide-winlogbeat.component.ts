194da1c7b4a0fd1d7ae1f7e02551761566fc70c3
Manuel Abascal <mjabascal10@gmail.com>
2024-06-11 23:17:21 +0300

    Fixed guide uninstall command

--- frontend/src/app/app-module/guides/guide-winlogbeat/guide-winlogbeat.component.ts
+++ frontend/src/app/app-module/guides/guide-winlogbeat/guide-winlogbeat.component.ts
@@ -42,7 +42,7 @@ export class GuideWinlogbeatComponent implements OnInit {
   }
   getUninstallCommand(): string {
     return `Start-Process "C:\\Program Files\\UTMStack\\UTMStack Agent\\utmstack_agent_installer.exe" -ArgumentList ` +
-            `uninstall' -NoNewWindow -Wait -ErrorAction SilentlyContinue | Out-Null; Start-Process -FilePath "sc.exe" ` +
+            `'uninstall' -NoNewWindow -Wait -ErrorAction SilentlyContinue | Out-Null; Start-Process -FilePath "sc.exe" ` +
             `-ArgumentList 'stop','UTMStackAgent' -Wait -ErrorAction SilentlyContinue | Out-Null; Start-Process -FilePath "sc.exe" ` +
             `-ArgumentList 'delete','UTMStackAgent' -Wait -ErrorAction SilentlyContinue | Out-Null; Start-Process -FilePath "sc.exe" ` +
             `-ArgumentList 'stop','UTMStackRedline' -Wait -ErrorAction SilentlyContinue | Out-Null; Start-Process -FilePath "sc.exe" ` +
