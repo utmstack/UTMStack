a1df8ee965e84834c95b27507c2b52519377d558
Yorjander Hernandez Vergara <99102374+Kbayero@users.noreply.github.com>
2024-07-01 16:44:11 +0300

    Fix unhandled errors (#732)

--- office365/processor/sendData.go
+++ office365/processor/sendData.go
@@ -4,12 +4,11 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/threatwinds/logger"
 	"github.com/utmstack/UTMStack/office365/configuration"
 	"github.com/utmstack/UTMStack/office365/utils"
 )
 
-func SendToCorrelation(data []TransformedLog) *logger.Error {
+func SendToCorrelation(data []TransformedLog) error {
 	utils.Logger.Info("uploading %d logs...", len(data))
 
 	for _, log := range data {
@@ -21,8 +20,10 @@ func SendToCorrelation(data []TransformedLog) *logger.Error {
 
 		_, status, e := utils.DoReq[map[string]interface{}](configuration.CORRELATIONURL, body, http.MethodPost, map[string]string{})
 		if e != nil {
+			utils.Logger.ErrorF("error sending log to correlation engine: %v", e)
 			continue
 		} else if status != http.StatusOK && status != http.StatusCreated {
+			utils.Logger.ErrorF("error sending log to correlation engine: status code %d", status)
 			continue
 		}
 
--- office365/utils/req.go
+++ office365/utils/req.go
@@ -3,18 +3,17 @@ package utils
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
-
-	"github.com/threatwinds/logger"
 )
 
-func DoReq[response any](url string, data []byte, method string, headers map[string]string) (response, int, *logger.Error) {
+func DoReq[response any](url string, data []byte, method string, headers map[string]string) (response, int, error) {
 	var result response
 
 	req, err := http.NewRequest(method, url, bytes.NewBuffer(data))
 	if err != nil {
-		return result, http.StatusInternalServerError, Logger.ErrorF(err.Error())
+		return result, http.StatusInternalServerError, err
 	}
 
 	for k, v := range headers {
@@ -25,22 +24,22 @@ func DoReq[response any](url string, data []byte, method string, headers map[str
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return result, http.StatusInternalServerError, Logger.ErrorF(err.Error())
+		return result, http.StatusInternalServerError, err
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return result, http.StatusInternalServerError, Logger.ErrorF(err.Error())
+		return result, http.StatusInternalServerError, err
 	}
 
 	if resp.StatusCode != http.StatusAccepted && resp.StatusCode != http.StatusOK {
-		return result, resp.StatusCode, Logger.ErrorF("while sending request to %s received status code: %d and response body: %s", url, resp.StatusCode, body)
+		return result, resp.StatusCode, fmt.Errorf("while sending request to %s received status code: %d and response body: %s", url, resp.StatusCode, body)
 	}
 
 	err = json.Unmarshal(body, &result)
 	if err != nil {
-		return result, http.StatusInternalServerError, Logger.ErrorF(err.Error())
+		return result, http.StatusInternalServerError, err
 	}
 
 	return result, resp.StatusCode, nil
