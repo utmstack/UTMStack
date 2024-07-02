748134ffc8a128cdb0e3b10a97c88ab749624ce8
Freddy R. Laffita Almaguer <c3s4rfred@gmail.com>
2024-07-03 00:33:09 +0300

    Fixing rules resource syntax error.

--- backend/src/main/java/com/park/utmstack/web/rest/correlation/rules/UtmCorrelationRulesResource.java
+++ backend/src/main/java/com/park/utmstack/web/rest/correlation/rules/UtmCorrelationRulesResource.java
@@ -56,7 +56,7 @@ public class UtmCorrelationRulesResource {
     /**
      * {@code POST  /correlation-rule} : Add a new correlation rule definition with its datatypes.
      *
-     * @param rulesVM the correlation rule definition to insert.
+     * @param utmCorrelationRulesDTO the correlation rule definition to insert.
      * @return the {@link ResponseEntity} with status {@code 204 (No Content)}, with status {@code 400 (Bad Request)}, or with status {@code 500 (Internal)} if errors occurred.
      */
     @PostMapping("/correlation-rule")
@@ -65,11 +65,6 @@ public class UtmCorrelationRulesResource {
         try {
             rulesService.save(this.utmCorrelationRulesMapper.toEntity(utmCorrelationRulesDTO));
             return ResponseEntity.noContent().build();
-        } catch (BadRequestException e) {
-            String msg = ctx + ": " + e.getLocalizedMessage();
-            log.error(msg);
-            applicationEventService.createEvent(msg, ApplicationEventType.ERROR);
-            return UtilResponse.buildErrorResponse(HttpStatus.BAD_REQUEST, msg);
         } catch (Exception e) {
             String msg = ctx + ": " + e.getLocalizedMessage();
             log.error(msg);
