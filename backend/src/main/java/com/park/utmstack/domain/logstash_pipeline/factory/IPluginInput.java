3695c91bbd3cfe8e1faac6ad51b58c7930cdf431
Freddy R. Laffita Almaguer <c3s4rfred@gmail.com>
2024-07-11 17:28:16 +0300

    Refactoring the pipelines flow, removing unused classes.

--- backend/src/main/java/com/park/utmstack/domain/logstash_pipeline/factory/IPluginInput.java
+++
@@ -1,7 +0,0 @@
-package com.park.utmstack.domain.logstash_pipeline.factory;
-
-import com.park.utmstack.domain.logstash_pipeline.types.InputConfiguration;
-
-public interface IPluginInput {
-    InputConfiguration getConfiguration() throws Exception;
-}
