103b0e049007c0a7187f9e7e9a9f6d4d39d211f5
Manuel Abascal <mjabascal10@gmail.com>
2024-07-12 01:29:19 +0300

    Adding search filter field to tenant config

--- backend/src/main/java/com/park/utmstack/repository/correlation/config/UtmTenantConfigRepository.java
+++ backend/src/main/java/com/park/utmstack/repository/correlation/config/UtmTenantConfigRepository.java
@@ -1,9 +1,18 @@
 package com.park.utmstack.repository.correlation.config;
 
+import com.park.utmstack.domain.correlation.config.UtmRegexPattern;
 import com.park.utmstack.domain.correlation.config.UtmTenantConfig;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface UtmTenantConfigRepository extends JpaRepository<UtmTenantConfig, Long>, JpaSpecificationExecutor<UtmTenantConfig> {
+    @Query(value = "SELECT t FROM UtmTenantConfig t WHERE" +
+            "(:search IS NULL OR ((t.assetName LIKE :search OR lower(t.assetName) LIKE lower(:search))))")
+    Page<UtmTenantConfig> searchByFilters(@Param("search") String search,
+                                          Pageable pageable);
 }
