169ff72689b8b491e48417ea37c3134dd5bfd7b9
Freddy R. Laffita Almaguer <c3s4rfred@gmail.com>
2024-08-01 23:58:52 +0300

    Remove unused fields in PipelineEvents

--- backend/src/main/java/com/park/utmstack/service/logstash_pipeline/response/pipeline/PipelineEvents.java
+++ backend/src/main/java/com/park/utmstack/service/logstash_pipeline/response/pipeline/PipelineEvents.java
@@ -1,34 +1,12 @@
 package com.park.utmstack.service.logstash_pipeline.response.pipeline;
 
 public class PipelineEvents {
-    Long in;
-    Long filtered;
     Long out;
 
     public PipelineEvents(){}
 
-    public PipelineEvents(Long in,
-                          Long filtered,
-                          Long out) {
-        this.in = in;
-        this.filtered = filtered;
+    public PipelineEvents(Long out) {
         this.out = out;
-    }
-
-    public Long getIn() {
-        return in;
-    }
-
-    public void setIn(Long in) {
-        this.in = in;
-    }
-
-    public Long getFiltered() {
-        return filtered;
-    }
-
-    public void setFiltered(Long filtered) {
-        this.filtered = filtered;
     }
 
     public Long getOut() {
