7c26a48f7aabaa69f34f24d7505ef8f3750fcdb9
Osmany Montero <osmontero@icloud.com>
2024-08-07 14:51:13 +0300

    preparing stats plugin

---
+++ plugins/stats/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"net"
+	"os"
+	"path"
+
+	"github.com/threatwinds/go-sdk/helpers"
+	"github.com/threatwinds/go-sdk/plugins"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type notificationServer struct {
+	plugins.UnimplementedNotificationServer
+}
+
+func main() {
+	os.Remove(path.Join(helpers.GetCfg().Env.Workdir,
+		"sockets", "com.utmstack.stats_notification.sock"))
+
+	laddr, err := net.ResolveUnixAddr(
+		"unix", path.Join(helpers.GetCfg().Env.Workdir,
+			"sockets", "com.utmstack.stats_notification.sock"))
+
+	if err != nil {
+		helpers.Logger().ErrorF(err.Error())
+		os.Exit(1)
+	}
+
+	listener, err := net.ListenUnix("unix", laddr)
+	if err != nil {
+		helpers.Logger().ErrorF(err.Error())
+		os.Exit(1)
+	}
+
+	grpcServer := grpc.NewServer()
+	plugins.RegisterNotificationServer(grpcServer, &notificationServer{})
+
+	if err := grpcServer.Serve(listener); err != nil {
+		helpers.Logger().ErrorF(err.Error())
+		os.Exit(1)
+	}
+}
+
+func (p *notificationServer) Notify(ctx context.Context, msg *plugins.Message) (*emptypb.Empty, error) {
+	helpers.Logger().Info("received message: %v", msg.Message)
+	
+	// implement statistics logic here
+
+	return &emptypb.Empty{}, nil
+}
