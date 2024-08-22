909037a3ecbe291f750c962666e9584ef018bf7e
Manuel Abascal <mjabascal10@gmail.com>
2024-08-22 09:06:00 -0500

    Optimized PDF report generation

--- web-pdf/src/main/java/com/utmstack/webtopdf/config/enums/AccessType.java
+++ web-pdf/src/main/java/com/utmstack/webtopdf/config/enums/AccessType.java
@@ -3,21 +3,21 @@ package com.utmstack.webtopdf.config.enums;
 import lombok.Getter;
 
 public enum AccessType {
-    UTM_TOKEN("Utm_Token", "?token=", "&url=dashboard/overview"),
+    UTM_TOKEN("Utm_Token", "?token=", "&url="),
     UTM_INTERNAL_KEY("Utm_Internal_Key", "?key=", "");
 
     @Getter
     private final String type;
-    private final String keyPrefix;
-    private final String additionalPart;
+    private final String accessType;
+    private final String url;
 
-    AccessType(String type, String keyPrefix, String additionalPart) {
+    AccessType(String type, String accessType, String url) {
         this.type = type;
-        this.keyPrefix = keyPrefix;
-        this.additionalPart = additionalPart;
+        this.accessType = accessType;
+        this.url = url;
     }
 
-    public String buildUrlPart(String accessKey) {
-        return keyPrefix + accessKey + additionalPart;
+    public String buildUrlPart(String accessKey, String route) {
+        return accessType + accessKey + url + route;
     }
 }
--- web-pdf/src/main/java/com/utmstack/webtopdf/service/PdfGenerationService.java
+++ web-pdf/src/main/java/com/utmstack/webtopdf/service/PdfGenerationService.java
@@ -3,16 +3,15 @@ package com.utmstack.webtopdf.service;
 import com.utmstack.webtopdf.config.WebDriverConfig;
 import com.utmstack.webtopdf.config.enums.AccessType;
 import lombok.extern.slf4j.Slf4j;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.Pdf;
-import org.openqa.selenium.PrintsPage;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.print.PageMargin;
 import org.openqa.selenium.print.PageSize;
 import org.openqa.selenium.print.PrintOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 @Service
 @Slf4j
@@ -31,29 +30,24 @@ public class PdfGenerationService {
         this.webDriverConfig = webDriverConfig;
     }
 
-    public byte[] generatePdf(String url, String accessKey, AccessType accessType) {
+    public byte[] generatePdf(String url, String route, String accessKey, AccessType accessType) {
 
+        String reportUrl = String.format("%s%s", url, accessType.buildUrlPart(accessKey, route));
         WebDriver webDriver = webDriverConfig.createWebDriver();
 
         try {
+            webDriver.get(reportUrl);
 
-            int indexAfterProtocol = url.indexOf('/', url.indexOf("//") + 2);
-            String front = (indexAfterProtocol != -1) ? url.substring(0, indexAfterProtocol) : url;
+            WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("span.text-primary .icon-spinner2")));
 
-            webDriver.get(front.concat(accessType.buildUrlPart(accessKey)));
-
-            TimeUnit.SECONDS.sleep(5);
-            webDriver.get(url);
-            TimeUnit.SECONDS.sleep(15);
             Pdf print = ((PrintsPage) webDriver).print(printOptions);
             webDriver.quit();
-
             return OutputType.BYTES.convertFromBase64Png(print.getContent());
-
         } catch (Exception e) {
-            log.error(e.getMessage());
+            log.error("Error generating PDF report: {}", e.getMessage(), e);
             webDriver.quit();
+            return new byte[0];
         }
-        return new byte[0];
     }
 }
