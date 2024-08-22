799293c05d18efe92bb261fdebee6c23c126c109
Freddy R. Laffita Almaguer <c3s4rfred@gmail.com>
2024-08-23 02:57:47 +0300

    Updating datatypes domain and repository classes.

--- backend/src/main/java/com/park/utmstack/repository/correlation/config/UtmDataTypesRepository.java
+++ backend/src/main/java/com/park/utmstack/repository/correlation/config/UtmDataTypesRepository.java
@@ -1,6 +1,5 @@
 package com.park.utmstack.repository.correlation.config;
 
-import com.park.utmstack.domain.collector.UtmCollector;
 import com.park.utmstack.domain.correlation.config.UtmDataTypes;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,8 +8,8 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 public interface UtmDataTypesRepository extends JpaRepository<UtmDataTypes, Long>, JpaSpecificationExecutor<UtmDataTypes> {
     @Query(nativeQuery = true, value = "SELECT nextval('utm_data_types_id_seq')")
@@ -22,5 +21,11 @@ public interface UtmDataTypesRepository extends JpaRepository<UtmDataTypes, Long
     Page<UtmDataTypes> searchByFilters(@Param("search") String search,
                                        Pageable pageable);
 
+    Optional<UtmDataTypes> findOneByDataType(String dataType);
+
+    List<UtmDataTypes> findAllByIncludedFalse();
+
+    @Query("select dt from UtmDataTypes dt where dt.systemOwner is false and dt.dataType not in (select ds.dataType from UtmDataInputStatus ds)")
+    List<UtmDataTypes> findOrphanDataSourceConfigurations();
 
 }
