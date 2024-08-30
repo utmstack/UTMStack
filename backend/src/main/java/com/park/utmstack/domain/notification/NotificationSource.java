094276836d63cdc584dd7f90c4bfbc116ab482c5
Manuel Abascal <mjabascal10@gmail.com>
2024-08-30 10:54:12 -0500

    Added RESTful Notification API

--- backend/src/main/java/com/park/utmstack/domain/notification/NotificationSource.java
+++ backend/src/main/java/com/park/utmstack/domain/notification/NotificationSource.java
@@ -3,5 +3,11 @@ package com.park.utmstack.domain.notification;
 public enum NotificationSource {
     AS400,
     BIT_DEFENDER,
+    AWS,
+    AZURE,
+    OFFICE_365,
+
+    SOPHOS,
+    GOOGLE,
     EMAIL_SETTING
 }
