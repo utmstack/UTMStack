8175905fc72539522a3c0d75151112f048fd3fce
Elena Lopez Milan <elopez@utmstack.com>
2024-11-20 13:19:20 -0600

    feat: compliance report view component

---
+++ frontend/src/app/compliance/shared/components/utm-cp-report/utm-cp-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-utm-cp-report',
+  templateUrl: './utm-cp-report.component.html',
+  styleUrls: ['./utm-cp-report.component.css']
+})
+export class UtmCpReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
