b489896f1126b20f882158221624d821c2503588
Manuel Abascal <mjabascal10@gmail.com>
2025-03-03 09:54:13 -0600

    fix: update sophos guide

--- backend/src/main/java/com/park/utmstack/domain/application_modules/factory/impl/ModuleSophos.java
+++ backend/src/main/java/com/park/utmstack/domain/application_modules/factory/impl/ModuleSophos.java
@@ -41,7 +41,7 @@ public class ModuleSophos implements IModule {
     public List<ModuleConfigurationKey> getConfigurationKeys(Long groupId) throws Exception {
         List<ModuleConfigurationKey> keys = new ArrayList<>();
 
-        // sophos_api_client_id
+        // sophos_api_url
         keys.add(ModuleConfigurationKey.builder()
             .withGroupId(groupId)
             .withConfKey("sophos_client_id")
@@ -56,7 +56,7 @@ public class ModuleSophos implements IModule {
             .withGroupId(groupId)
             .withConfKey("sophos_x_api_key")
             .withConfName("Client Secret")
-            .withConfDescription("Configure Sophos Central Client Secret")
+            .withConfDescription("Configure Sophos Central client secret")
             .withConfDataType("password")
             .withConfRequired(true)
             .build());
