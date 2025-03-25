9a1fca5d32595015544fbe0f66dfd29d45a2afcd
Manuel Abascal <mjabascal10@gmail.com>
2025-03-25 14:23:33 -0500

    feat: add organization name configuration parameter

--- backend/src/main/java/com/park/utmstack/service/mail_config/MailConfigService.java
+++ backend/src/main/java/com/park/utmstack/service/mail_config/MailConfigService.java
@@ -18,7 +18,7 @@ public class MailConfigService {
         mailConfig.setUsername(getParamValue(parameters, Constants.PROP_MAIL_USERNAME));
         mailConfig.setPassword(getParamValue(parameters, Constants.PROP_MAIL_PASSWORD));
         mailConfig.setAuthType(getParamValue(parameters, Constants.PROP_MAIL_SMTP_AUTH));
-        mailConfig.setFrom(String.valueOf(new InternetAddress(Constants.CFG.get(Constants.PROP_MAIL_FROM), getParamValue(parameters, Constants.PROP_MAIL_ORGNAME))));
+        mailConfig.setFrom(String.valueOf(new InternetAddress(getParamValue(parameters, Constants.PROP_MAIL_FROM), getParamValue(parameters, Constants.PROP_MAIL_ORGNAME))));
         mailConfig.setPort(Integer.parseInt(getParamValue(parameters, Constants.PROP_MAIL_PORT)));
 
         return mailConfig;
