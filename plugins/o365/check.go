70bdb419acf5cc402c75d423833d61908b36f6a2
JocLRojas <joc.l.rojas02@gmail.com>
2025-03-26 17:08:53 -0400

    Add connection checker utility for Office 365 module.

---
+++ plugins/o365/check.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/threatwinds/go-sdk/catcher"
+)
+
+const (
+	connectionTimeout = 5 * time.Second
+	wait              = 3 * time.Second
+)
+
+func ConnectionChecker(url string) error {
+	checkConn := func() error {
+		ctx, cancel := context.WithTimeout(context.Background(), connectionTimeout)
+		defer cancel()
+
+		if err := checkConnection(url, ctx); err != nil {
+			return fmt.Errorf("connection failed")
+		}
+		return nil
+	}
+
+	if err := infiniteRetryIfXError(checkConn, "connection failed"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func checkConnection(url string, ctx context.Context) error {
+	client := &http.Client{}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func infiniteRetryIfXError(f func() error, exception string) error {
+	var xErrorWasLogged bool
+
+	for {
+		err := f()
+		if err != nil && is(err, exception) {
+			if !xErrorWasLogged {
+				catcher.Error("An error occurred (%s), will keep retrying indefinitely...", err, nil)
+				xErrorWasLogged = true
+			}
+			time.Sleep(wait)
+			continue
+		}
+
+		return err
+	}
+}
+
+func is(e error, args ...string) bool {
+	for _, arg := range args {
+		if strings.Contains(e.Error(), arg) {
+			return true
+		}
+	}
+	return false
+}
