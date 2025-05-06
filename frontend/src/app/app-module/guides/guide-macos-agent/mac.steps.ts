743fb1913983404b59a1ea8ccb6eaf9e656a7eeb
Manuel Abascal <mjabascal10@gmail.com>
2025-05-06 14:50:31 -0500

    feat: update macOS install steps with `utmstack-macos-agent.pkg`

--- frontend/src/app/app-module/guides/guide-macos-agent/mac.steps.ts
+++ frontend/src/app/app-module/guides/guide-macos-agent/mac.steps.ts
@@ -2,13 +2,17 @@ import {Step} from '../shared/step';
 
 export const MAC_STEPS: Step[] = [
   {id: '1',
-    name: 'Reach out to support to request the installation dependencies for macOS. ' +
-      'These are required to proceed with the installation or uninstallation process.',
+    name: 'Contact UTMStack support to obtain the <strong> `utmstack-macos-agent.pkg` </strong> file. ' +
+      'This package is required to download and install the necessary dependencies.'
   },
-  {id: '2',
+  {
+    id: '2',
+    name: 'Run the <strong> `utmstack-macos-agent.pkg` file. This will download and install the required components for the UTMStack agent.'
+  },
+  {id: '3',
     name: 'Use the following command according to the action you wish to perform (install or uninstall):',
     content: {
-      id: 'stepContent2'
+      id: 'stepContent3'
     }
   },
 ];
