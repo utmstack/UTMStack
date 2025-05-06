85673c6a07ff392e961c11f0441269d666146b5f
Manuel Abascal <mjabascal10@gmail.com>
2025-05-06 14:53:48 -0500

    feat: update macOS install steps with `utmstack-macos-agent.pkg`

--- frontend/src/app/app-module/guides/guide-macos-agent/guide-macos-agent.component.ts
+++ frontend/src/app/app-module/guides/guide-macos-agent/guide-macos-agent.component.ts
@@ -2,7 +2,9 @@ import {Component, Input, OnInit} from '@angular/core';
 import {UtmModulesEnum} from '../../shared/enum/utm-module.enum';
 import {Step} from '../shared/step';
 import {MAC_STEPS} from './mac.steps';
-import {PLATFORMS} from "../shared/constant";
+import {
+  FederationConnectionService
+} from "../../../app-management/connection-key/shared/services/federation-connection.service";
 
 @Component({
   selector: 'app-guide-macos-agent',
@@ -15,13 +17,49 @@ export class GuideMacosAgentComponent implements OnInit {
   @Input() filebeatModuleName: string;
   module = UtmModulesEnum;
   @Input() serverId: number;
-  platforms = PLATFORMS;
 
   steps: Step[] = MAC_STEPS;
-  @Input() dataType: string;
-  constructor() { }
+  architectures = [];
+  token: string;
+  constructor(private federationConnectionService: FederationConnectionService,) { }
 
   ngOnInit() {
-    console.log(this.steps);
+    this.getToken();
+  }
+
+
+  getToken() {
+    this.federationConnectionService.getToken().subscribe(response => {
+      if (response.body !== null && response.body !== '') {
+        this.token = response.body;
+      } else {
+        this.token = '';
+      }
+      this.loadArchitectures();
+    });
+  }
+
+  getCommandARM(installerName: string): string {
+    const ip = window.location.host.includes(':') ? window.location.host.split(':')[0] : window.location.host;
+
+    return `sudo bash -c "/opt/utmstack/${installerName} ${ip} <secret>${this.token}</secret> yes"`;
+  }
+
+
+  getUninstallCommand(installerName: string): string {
+    // tslint:disable-next-line:max-line-length
+    return `sudo bash -c "/opt/utmstack/${installerName} uninstall; launchctl bootout system /Library/LaunchDaemons/UTMStackAgent.plist 2>/dev/null; rm /Library/LaunchDaemons/UTMStackAgent.plist; rm -rf /opt/utmstack"`;
+  }
+
+
+  private loadArchitectures() {
+    this.architectures = [
+      {
+        id: 1, name: 'ARM64',
+        install: this.getCommandARM('utmstack_agent_service install'),
+        uninstall: this.getUninstallCommand('utmstack_agent_service install'),
+        shell: ''
+      },
+    ];
   }
 }
--- frontend/src/app/app-module/guides/guide-macos-agent/mac.steps.ts
+++ frontend/src/app/app-module/guides/guide-macos-agent/mac.steps.ts
@@ -1,43 +1,18 @@
-import {COLLECTOR_MESSAGE, Step} from '../shared/step';
+import {Step} from '../shared/step';
 
 export const MAC_STEPS: Step[] = [
-  {id: '1', name: 'Install Homebrew, using the official documentation\n' +
-      '          <a class="text-primary font-weight-semibold"\n' +
-      '             href="https://docs.brew.sh/Installation"\n' +
-      '             target="_blank">here</a>, if you already installed go to the next step.'},
-  {id: '2', name: 'Install rsyslog on MacOS:',
+  {id: '1',
+    name: 'Contact UTMStack support to obtain the <strong> `utmstack-macos-agent.pkg` </strong> file. ' +
+      'This package is required to download and install the necessary dependencies.'
+  },
+  {
+    id: '2',
+    name: 'Run the <strong> `utmstack-macos-agent.pkg` file. This will download and install the required components for the UTMStack agent.'
+  },
+  {id: '3',
+    name: 'Use the following command according to the action you wish to perform (install or uninstall):',
     content: {
-      id: 'stepContent2',
-      commands: ['brew install rsyslog']
+      id: 'stepContent3'
     }
   },
-  {id: '3', name: 'Open the file /etc/syslog.conf in an editor:\n',
-    content: {
-      id: 'stepContent3',
-      commands: ['sudo nano /etc/syslog.conf']
-    }
-  },
-  {id: '4', name: 'Append the following line at the end if you want to send over TPC:\n',
-    content: {
-      id: 'stepContent4',
-      commands: ['*.* @@IP_OF_YOUR_UTMSTACK_AGENT:7015']
-    }
-  },
-  {id: '5', name: 'Restart the syslog daemon:',
-    content: {
-      id: 'stepContent5',
-      commands: ['sudo launchctl stop /System/Library/LaunchDaemons/com.apple.syslogd.plist',
-                 'sudo launchctl start /System/Library/LaunchDaemons/com.apple.syslogd.plist']
-    }
-  },
-  {id: '6', name: COLLECTOR_MESSAGE,
-    content: {
-      id: 'stepContent6'
-    }
-  },
-  {id: '7', name: 'Click on the button shown below, to activate the UTMStack features related to this integration',
-    content: {
-      id: 'stepContent7'
-    }
-  }
 ];
