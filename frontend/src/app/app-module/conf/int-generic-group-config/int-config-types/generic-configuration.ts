39dd8c1c64d9cb894778bab4b35808320fd1fad1
Manuel Abascal <mjabascal10@gmail.com>
2025-05-07 10:22:05 -0500

    chore: resolve merge conflicts

--- frontend/src/app/app-module/conf/int-generic-group-config/int-config-types/generic-configuration.ts
+++ frontend/src/app/app-module/conf/int-generic-group-config/int-config-types/generic-configuration.ts
@@ -1,37 +1,32 @@
-import {HttpResponse} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {catchError, finalize, map, switchMap, tap} from 'rxjs/operators';
 import { UtmModuleGroupType } from 'src/app/app-module/shared/type/utm-module-group.type';
-import {ModalService} from '../../../../core/modal/modal.service';
 import {UtmToastService} from '../../../../shared/alert/utm-toast.service';
-import {EncryptService} from '../../../../shared/services/util/encrypt.service';
-import {ModuleChangeStatusBehavior} from '../../../shared/behavior/module-change-status.behavior';
-import {UtmModulesEnum} from '../../../shared/enum/utm-module.enum';
-import {UtmModuleCollectorService} from '../../../shared/services/utm-module-collector.service';
-import {UtmModuleGroupConfService} from '../../../shared/services/utm-module-group-conf.service';
 import {UtmModuleGroupService} from '../../../shared/services/utm-module-group.service';
-import {UtmListCollectorType} from '../../../shared/type/utm-list-collector-type';
-import {UtmModuleCollectorType} from '../../../shared/type/utm-module-collector.type';
+
 import {IntegrationConfig} from './integration-config';
 
 @Injectable()
 export class GenericConfiguration extends IntegrationConfig {
 
     constructor(private utmModuleGroupService: UtmModuleGroupService,
-                private toast: UtmToastService,
-                private encryptService: EncryptService,
-                private utmModuleGroupConfService: UtmModuleGroupConfService,
-                private modalService: ModalService,
-                private moduleChangeStatusBehavior: ModuleChangeStatusBehavior) {
+                private toast: UtmToastService) {
         super();
     }
 
     getIntegrationConfigs(moduleId: number) {
         return this.utmModuleGroupService.query({moduleId}).pipe(
             tap(response => {
-                this.groups = response.body;
-                this.moduleId = moduleId;
+              this.groups = response.body.map(group => {
+                group.moduleGroupConfigurations.forEach(config => {
+                  if (config.confOptions) {
+                    config.confOptions = JSON.parse(config.confOptions);
+                  }
+                });
+                return group;
+              });
+              this.moduleId = moduleId;
             }),
             catchError(error => {
                 this.toast.showError('Error listing configurations',
--- frontend/src/app/app-module/guides/guide-macos-agent/mac.steps.ts
+++ frontend/src/app/app-module/guides/guide-macos-agent/mac.steps.ts
@@ -7,7 +7,8 @@ export const MAC_STEPS: Step[] = [
   },
   {
     id: '2',
-    name: 'Run the <strong> `utmstack-macos-agent.pkg` file. This will download and install the required components for the UTMStack agent.'
+    name: 'Run the <strong> `utmstack-macos-agent.pkg`</strong> file. ' +
+      'This will download and install the required components for the UTMStack agent.'
   },
   {id: '3',
     name: 'Use the following command according to the action you wish to perform (install or uninstall):',
