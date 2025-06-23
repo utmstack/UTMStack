f5751519880859ace709d5ee2785dd65078d6644
Yorjander Hernandez Vergara <yorjaKbayero@gmail.com>
2025-06-23 11:21:56 -0400

    Allow agent dependencies download using powershell with tls v1.3

--- frontend/src/app/app-module/guides/guide-winlogbeat/guide-winlogbeat.component.ts
+++ frontend/src/app/app-module/guides/guide-winlogbeat/guide-winlogbeat.component.ts
@@ -56,9 +56,8 @@ export class GuideWinlogbeatComponent implements OnInit {
     const ip = window.location.host.includes(':') ? window.location.host.split(':')[0] : window.location.host;
 
     return `New-Item -ItemType Directory -Force -Path "C:\\Program Files\\UTMStack\\UTMStack Agent"; ` +
-      `& curl.exe -k -H "connection-key: <secret>${this.token}</secret>" ` +
-      `-o "C:\\Program Files\\UTMStack\\UTMStack Agent\\${arch}" ` +
-      `"https://${ip}:9001/private/dependencies/agent/${arch}"; ` +
+      `Invoke-WebRequest -Uri "https://${ip}:9001/private/dependencies/agent/${arch}" ` +
+      `-OutFile "C:\\Program Files\\UTMStack\\UTMStack Agent\\${arch}" -SkipCertificateCheck; ` +
       `Start-Process "C:\\Program Files\\UTMStack\\UTMStack Agent\\${arch}" ` +
       `-ArgumentList 'install', '${ip}', '<secret>${this.token}</secret>', 'yes' -NoNewWindow -Wait`;
   }
