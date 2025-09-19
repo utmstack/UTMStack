9c8026eb576a7e51b00e2b24623dee66d7642676
JocLRojas <joc.l.rojas02@gmail.com>
2025-09-19 18:32:47 -0400

    Update log writing with correct use of 'catcher' in the office365 and sophos file env.go

--- sophos/utils/env.go
+++ sophos/utils/env.go
@@ -1,18 +1,21 @@
 package utils
 
 import (
-	"log"
 	"os"
+
+	"github.com/threatwinds/go-sdk/catcher"
 )
 
 // Getenv returns the environment variable
 func Getenv(key string) string {
 	value, defined := os.LookupEnv(key)
 	if !defined {
-		log.Fatalf("Error loading environment variable: %s: environment variable does not exist\n", key)
+		catcher.Error("Error loading environment variable, environment variable does not exist", nil, map[string]any{"key": key})
+		os.Exit(1)
 	}
 	if (value == "") || (value == " ") {
-		log.Fatalf("Error loading environment variable: %s: empty environment variable\n", key)
+		catcher.Error("Error loading environment variable, empty environment variable", nil, map[string]any{"key": key})
+		os.Exit(1)
 	}
 	return value
 }
