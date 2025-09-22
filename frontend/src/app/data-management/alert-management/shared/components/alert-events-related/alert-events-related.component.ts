564c6df538eb6f17ff68c6f2f741f93229f7b404
Manuel Abascal <mjabascal10@gmail.com>
2025-09-22 11:03:04 -0500

    feat(alerts): add alert events related component with pagination and sorting features

---
+++ frontend/src/app/data-management/alert-management/shared/components/alert-events-related/alert-events-related.component.ts
@@ -0,0 +1,81 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {
+  UtmTableDetailComponent
+} from '../../../../../shared/components/utm/table/utm-table/utm-table-detail/utm-table-detail.component';
+import {LOG_ANALYZER_TOTAL_ITEMS} from '../../../../../shared/constants/log-analyzer.constant';
+import {ITEMS_PER_PAGE} from '../../../../../shared/constants/pagination.constants';
+import {ElasticDataTypesEnum} from '../../../../../shared/enums/elastic-data-types.enum';
+import {UtmFieldType} from '../../../../../shared/types/table/utm-field.type';
+
+@Component({
+  selector: 'app-alert-events-related',
+  templateUrl: './alert-events-related.component.html',
+  styleUrls: ['./alert-events-related.component.scss']
+})
+export class AlertEventsRelatedComponent implements OnInit {
+
+  fields: UtmFieldType[]  = [
+    {field: 'timestamp', label: '@timestamp', visible: true, type: ElasticDataTypesEnum.DATE},
+  ];
+  @Input() events: any[] = [];
+  displayedLogs: any[] = [];
+  page = 1;
+  readonly totalItems = LOG_ANALYZER_TOTAL_ITEMS;
+  itemsPerPage = ITEMS_PER_PAGE;
+  readonly componentDetail = UtmTableDetailComponent;
+  sortField = '';
+  sortDirection: 'asc' | 'desc' = 'asc';
+
+  constructor() { }
+
+  ngOnInit() {
+    for (let i = 0; i < 100; i++) {
+      const baseDate = new Date(this.events[0].timestamp);
+
+      baseDate.setDate(baseDate.getDate() + Math.floor(i / 25));
+
+      this.events.push({
+        ...this.events[0],
+        timestamp: baseDate.getTime().toString()
+      });
+    }
+    this.applyFilters();
+  }
+
+
+  onPageChange($event: number) {
+    this.page = $event;
+    this.applyFilters();
+  }
+
+  onRemoveColumn($event: UtmFieldType) {
+
+  }
+
+  onSizeChange($event: number) {
+    this.itemsPerPage = $event;
+    this.applyFilters();
+  }
+
+  onSortBy($event: string) {
+
+  }
+
+  applyFilters() {
+    const size = this.itemsPerPage;
+    const filtered = [...this.events];
+
+    // Sorting
+    if (this.sortField) {
+      filtered.sort((a, b) => {
+        if (a[this.sortField] < b[this.sortField]) { return this.sortDirection === 'asc' ? -1 : 1; }
+        if (a[this.sortField] > b[this.sortField]) { return this.sortDirection === 'asc' ? 1 : -1; }
+        return 0;
+      });
+    }
+
+    // Pagination
+    const start = (this.page - 1) * size;
+    this.displayedLogs = filtered.slice(start, start + size);
+  }
+}
