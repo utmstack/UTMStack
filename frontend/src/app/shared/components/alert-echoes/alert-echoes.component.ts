84b064643b477be2a3fcef72f29780691161929a
Manuel Abascal <mjabascal10@gmail.com>
2025-09-23 17:09:12 -0500

    feat(alerts): add alert echoes component with basic template and styles

---
+++ frontend/src/app/shared/components/alert-echoes/alert-echoes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-echoes',
+  templateUrl: './alert-echoes.component.html',
+  styleUrls: ['./alert-echoes.component.scss']
+})
+export class AlertEchoesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
