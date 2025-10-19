cb201834519823cf3760d72e945fe83cc98d3f96
Manuel Abascal <mjabascal10@gmail.com>
2025-10-19 10:07:52 -0500

    feat(api_keys): implement API key filtering and usage logging for enhanced security

--- backend/src/main/java/com/park/utmstack/security/api_key/ApiKeyFilter.java
+++ backend/src/main/java/com/park/utmstack/security/api_key/ApiKeyFilter.java
@@ -52,6 +52,7 @@ public class ApiKeyFilter extends OncePerRequestFilter {
     private final ApiKeyService apiKeyService;
     private final ConcurrentMap<String, Boolean> invalidApiKeyBlackList = new ConcurrentHashMap<>();
     private final ConcurrentMap<String, SubnetUtils> cidrCache = new ConcurrentHashMap<>();
+    private final ApplicationEventService applicationEventService;
     private final ApiKeyUsageLoggingService apiKeyUsageLoggingService;
 
     @Override
