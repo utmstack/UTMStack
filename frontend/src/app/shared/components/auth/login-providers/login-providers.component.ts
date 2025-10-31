3c336575544c3574d434b2c57d3eb540583a6f07
Manuel Abascal <mjabascal10@gmail.com>
2025-10-31 08:56:10 -0500

    feat(login): add login providers component and service
    Signed-off-by: Manuel Abascal <mjabascal10@gmail.com>

--- frontend/src/app/shared/components/auth/login-providers/login-providers.component.ts
+++ frontend/src/app/shared/components/auth/login-providers/login-providers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IdentityProviderDto, LoginProviderService} from '../../../services/login-provider.service';
 
 @Component({
   selector: 'app-login-providers',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginProvidersComponent implements OnInit {
 
-  constructor() { }
+  request = {
+    page: 0,
+    size: 10
+  };
+
+  providers: IdentityProviderDto[] = [] as IdentityProviderDto[];
+
+  constructor(private loginProviderService: LoginProviderService) { }
 
   ngOnInit() {
+    this.loadAllProviders();
   }
 
+  loadAllProviders() {
+    this.loginProviderService.getAllProviders(this.request).subscribe(
+      response => {
+        console.log('Login Providers:', response.body);
+        this.providers = response.body || [];
+      },
+      error => {
+        console.error('Error fetching login providers:', error);
+        this.providers = [];
+      }
+    );
+  }
+
+  loginWithProvider(provider: IdentityProviderDto) {
+    if (!provider.active) {
+      console.warn(`Provider ${provider.name} is inactive.`);
+      return;
+    }
+
+    this.loginProviderService.loginWithProvider(provider.providerType.toLowerCase());
+  }
 }
