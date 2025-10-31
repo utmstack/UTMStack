71f07d6da237b2d0bd80ca506cf9d2612e4bcb48
Manuel Abascal <mjabascal10@gmail.com>
2025-10-31 08:56:10 -0500

    feat(login): add login providers component and service
    Signed-off-by: Manuel Abascal <mjabascal10@gmail.com>

--- frontend/src/app/shared/services/login-provider.service.ts
+++ frontend/src/app/shared/services/login-provider.service.ts
@@ -2,6 +2,30 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {SERVER_API_URL} from '../../app.constants';
 import {createRequestOption} from '../util/request-util';
+import {Observable} from "rxjs";
+
+export enum ProviderType {
+  GOOGLE = 'GOOGLE',
+  AZURE = 'AZURE',
+  OKTA = 'OKTA',
+  GITHUB = 'GITHUB',
+  AUTH0 = 'AUTH0',
+  FACEBOOK = 'FACEBOOK',
+  LINKEDIN = 'LINKEDIN'
+}
+
+export interface IdentityProviderDto {
+  id: number;
+  name: string;
+  providerType: ProviderType;
+  authUri: string;
+  tokenUri: string;
+  redirectUri: string;
+  scopes: string;
+  allowedDomains?: string;
+  active: boolean;
+}
+
 
 @Injectable({providedIn: 'root'})
 export class LoginProviderService {
@@ -11,6 +35,11 @@ export class LoginProviderService {
 
   getAllProviders(request: any) {
     const params = createRequestOption(request);
-    return this.http.get(this.serverApiUrl, {params, observe: 'response'});
+    return this.http.get<IdentityProviderDto[]>(this.serverApiUrl, {params, observe: 'response'});
+  }
+
+  loginWithProvider(provider: string): void {
+    console.log(`${SERVER_API_URL}oauth2/authorization/${provider}`);
+    window.location.href = `${SERVER_API_URL}oauth2/authorization/${provider}`;
   }
 }
