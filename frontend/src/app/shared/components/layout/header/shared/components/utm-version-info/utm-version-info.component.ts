be8242598a6697f4551562cb7f8d689566100e64
Manuel Abascal <mjabascal10@gmail.com>
2025-11-26 11:23:50 -0600

    feat(header): integrate version info display and update logic
    Signed-off-by: Manuel Abascal <mjabascal10@gmail.com>

--- frontend/src/app/shared/components/layout/header/shared/components/utm-version-info/utm-version-info.component.ts
+++ frontend/src/app/shared/components/layout/header/shared/components/utm-version-info/utm-version-info.component.ts
@@ -1,49 +1,15 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {EMPTY, Observable, Subject} from 'rxjs';
-import {catchError, map, tap} from 'rxjs/operators';
+import {Component, Input, OnInit} from '@angular/core';
 import {UtmToastService} from '../../../../../../alert/utm-toast.service';
 import {CheckForUpdatesService} from '../../../../../../services/updates/check-for-updates.service';
-import {VersionType, VersionTypeService} from '../../../../../../services/util/version-type.service';
-import {AppVersionInfo, VersionInfo} from '../../../../../../types/updates/updates.type';
+import {AppVersionInfo} from '../../../../../../types/updates/updates.type';
 
 @Component({
   selector: 'app-utm-version-info',
   templateUrl: './utm-version-info.component.html',
   styleUrls: ['./utm-version-info.component.css']
 })
-export class UtmVersionInfoComponent implements OnInit {
-  versionInfo: AppVersionInfo;
+export class UtmVersionInfoComponent {
+ @Input('version') versionInfo: AppVersionInfo;
 
-  constructor(private checkForUpdatesService: CheckForUpdatesService,
-              private utmToastService: UtmToastService,
-              private versionTypeService: VersionTypeService) {
-  }
-
-  ngOnInit() {
-    this.checkForUpdatesService.getVersion()
-      .pipe(
-        map(response => response.body || null),
-        tap((versionInfo: AppVersionInfo) => {
-          const version = versionInfo && versionInfo.version || '';
-          const versionType = versionInfo.edition.includes('community') || version === ''
-            ? VersionType.COMMUNITY
-            : VersionType.ENTERPRISE;
-
-          if (versionType !== this.versionTypeService.versionType()) {
-            this.versionTypeService.changeVersionType(versionType);
-          }
-        }),
-        catchError(() => {
-          this.utmToastService.showError(
-            'Error fetching version info',
-            'An error occurred while fetching version info.'
-          );
-          return EMPTY;
-        })
-      )
-      .subscribe(versionInfo => {
-        this.versionInfo = versionInfo;
-      });
-  }
-
+  constructor() {}
 }
