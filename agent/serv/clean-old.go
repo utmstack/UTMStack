036e3e1ac045d01371e57907ab5c8081606f0983
Yadian Llada Lopez <yadian.llada@gmail.com>
2025-12-18 08:48:51 -0500

    feat(agent): implement updater service installation and management

--- agent/serv/clean-old.go
+++ agent/serv/clean-old.go
@@ -1,76 +1,30 @@
 package serv
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"runtime"
-
 	"github.com/utmstack/UTMStack/agent/config"
 	"github.com/utmstack/UTMStack/agent/utils"
 )
 
 func CleanOldServices(cnf *config.Config) {
-	oldVersion := false
-
-	isUpdaterINstalled, err := utils.CheckIfServiceIsInstalled("UTMStackUpdater")
-	if err != nil {
-		utils.Logger.LogF(100, "error checking if service is installed: %v", err)
-	}
-
-	if isUpdaterINstalled {
-		oldVersion = true
-		err = utils.StopService("UTMStackUpdater")
-		if err != nil {
-			utils.Logger.LogF(100, "error stopping service: %v", err)
-		}
-
-		err = utils.UninstallService("UTMStackUpdater")
-		if err != nil {
-			utils.Logger.LogF(100, "error uninstalling service: %v", err)
-		}
-	}
-
 	isRedlineInstalled, err := utils.CheckIfServiceIsInstalled("UTMStackRedline")
 	if err != nil {
-		utils.Logger.LogF(100, "error checking if service is installed: %v", err)
+		utils.Logger.LogF(100, "error checking if UTMStackRedline service is installed: %v", err)
+		return
 	}
 
 	if isRedlineInstalled {
-		oldVersion = true
+		utils.Logger.Info("old UTMStackRedline service found, removing...")
+
 		err = utils.StopService("UTMStackRedline")
 		if err != nil {
-			utils.Logger.LogF(100, "error stopping service: %v", err)
+			utils.Logger.LogF(100, "error stopping UTMStackRedline service: %v", err)
 		}
 
 		err = utils.UninstallService("UTMStackRedline")
 		if err != nil {
-			utils.Logger.LogF(100, "error uninstalling service: %v", err)
+			utils.Logger.LogF(100, "error uninstalling UTMStackRedline service: %v", err)
+		} else {
+			utils.Logger.Info("UTMStackRedline service removed successfully")
 		}
-	}
-
-	if oldVersion {
-		utils.Logger.Info("old version of agent found, downloading new version")
-		if runtime.GOOS != "darwin" {
-			if err := utils.DownloadFile(fmt.Sprintf(config.DependUrl, cnf.Server, config.DependenciesPort, fmt.Sprintf(config.UpdaterSelf, "")), map[string]string{}, fmt.Sprintf(config.UpdaterSelf, "_new"), utils.GetMyPath(), cnf.SkipCertValidation); err != nil {
-				utils.Logger.LogF(100, "error downloading updater: %v", err)
-				return
-			}
-		}
-
-		oldFilePath := filepath.Join(utils.GetMyPath(), fmt.Sprintf(config.UpdaterSelf, ""))
-		newFilePath := filepath.Join(utils.GetMyPath(), fmt.Sprintf(config.UpdaterSelf, "_new"))
-
-		utils.Logger.LogF(100, "renaming %s to %s", newFilePath, oldFilePath)
-		err := os.Remove(oldFilePath)
-		if err != nil {
-			utils.Logger.LogF(100, "error removing old updater: %v", err)
-		}
-		err = os.Rename(newFilePath, oldFilePath)
-		if err != nil {
-			utils.Logger.LogF(100, "error renaming updater: %v", err)
-		}
-	} else {
-		utils.Logger.LogF(100, "no old version of agent found")
 	}
 }
--- agent/serv/service.go
+++ agent/serv/service.go
@@ -2,8 +2,11 @@ package serv
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"runtime"
 	"strconv"
 	"syscall"
 
@@ -36,6 +39,9 @@ func (p *program) run() {
 		utils.Logger.Fatal("error getting config: %v", err)
 	}
 
+	// Ensure updater service is installed
+	ensureUpdaterServiceInstalled()
+
 	CleanOldServices(cnf)
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -59,4 +65,53 @@ func (p *program) run() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 	<-signals
+}
+
+func ensureUpdaterServiceInstalled() {
+	isInstalled, err := utils.CheckIfServiceIsInstalled("UTMStackUpdater")
+	if err != nil {
+		utils.Logger.ErrorF("error checking if updater service is installed: %v", err)
+		return
+	}
+
+	if isInstalled {
+		utils.Logger.Info("updater service is already installed")
+		return
+	}
+
+	utils.Logger.Info("updater service not found, installing...")
+
+	updaterPath := filepath.Join(utils.GetMyPath(), fmt.Sprintf(config.UpdaterFile, ""))
+
+	if !utils.CheckIfPathExist(updaterPath) {
+
+		cnf, err := config.GetCurrentConfig()
+		if err != nil {
+			utils.Logger.ErrorF("error getting config to download updater: %v", err)
+			return
+		}
+
+		updaterBinary := fmt.Sprintf(config.UpdaterFile, "")
+		if err := utils.DownloadFile(fmt.Sprintf(config.DependUrl, cnf.Server, config.DependenciesPort, updaterBinary), map[string]string{}, updaterBinary, utils.GetMyPath(), cnf.SkipCertValidation); err != nil {
+			utils.Logger.ErrorF("error downloading updater binary: %v", err)
+			return
+		}
+
+		if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+			if err := utils.Execute("chmod", utils.GetMyPath(), "755", updaterBinary); err != nil {
+				utils.Logger.ErrorF("error setting permissions on updater: %v", err)
+				return
+			}
+		}
+
+		utils.Logger.Info("updater binary downloaded successfully")
+	}
+
+	err = utils.Execute(updaterPath, utils.GetMyPath(), "install")
+	if err != nil {
+		utils.Logger.ErrorF("error installing updater service: %v", err)
+		return
+	}
+
+	utils.Logger.Info("updater service installed successfully")
 }
