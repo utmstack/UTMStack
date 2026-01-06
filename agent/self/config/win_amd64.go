865588be39a60189c6586506c7f6bad425ae2d64
Yadian Llada Lopez <yadian.llada@gmail.com>
2026-01-05 19:39:16 -0500

    refactor(agent): remove self

--- agent/self/config/win_amd64.go
+++
@@ -1,8 +0,0 @@
-//go:build windows && amd64
-// +build windows,amd64
-
-package config
-
-var (
-	ServiceFile = "utmstack_agent_service%s.exe"
-)
--- agent/self/config/win_arm64.go
+++
@@ -1,8 +0,0 @@
-//go:build windows && arm64
-// +build windows,arm64
-
-package config
-
-var (
-	ServiceFile = "utmstack_agent_service_arm64%s.exe"
-)
