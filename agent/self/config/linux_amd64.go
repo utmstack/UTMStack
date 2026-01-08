87cd2889bcdaf65cd290d1874bec17d22ed29369
Yadian Llada Lopez <yadian.llada@gmail.com>
2026-01-08 13:47:04 -0500

    refactor: remove unused agent self

--- agent/self/config/linux_amd64.go
+++
@@ -1,8 +0,0 @@
-//go:build linux && amd64
-// +build linux,amd64
-
-package config
-
-var (
-	ServiceFile = "utmstack_agent_service%s"
-)
--- agent/self/config/macos.go
+++
@@ -1,8 +0,0 @@
-//go:build darwin
-// +build darwin
-
-package config
-
-var (
-	ServiceFile = "utmstack_agent_service%s"
-)
