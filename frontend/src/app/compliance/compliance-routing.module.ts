1f96401856946ab330b62d0f145a7681107ba894
Elena Lopez Milan <elopez@utmstack.com>
2026-01-19 13:04:53 -0600

    feat: Add new ControlConfig creation component for SQL‑based compliance evaluation

--- frontend/src/app/compliance/compliance-routing.module.ts
+++ frontend/src/app/compliance/compliance-routing.module.ts
@@ -4,11 +4,11 @@ import {UserRouteAccessService} from '../core/auth/user-route-access-service';
 import {ADMIN_ROLE, USER_ROLE} from '../shared/constants/global.constant';
 import {ComplianceCustomViewComponent} from './compliance-custom-view/compliance-custom-view.component';
 import {CpStandardManagementComponent} from './compliance-management/cp-standard-management/cp-standard-management.component';
+import {ComplianceReportViewerComponent} from './compliance-report-viewer/compliance-report-viewer.component';
+import { CompliancePrintViewComponent } from './compliance-reports-view/components/compliance-print-view/compliance-print-view.component';
 import {ComplianceResultViewComponent} from './compliance-result-view/compliance-result-view.component';
 import {ComplianceScheduleComponent} from './compliance-schedule/compliance-schedule.component';
 import {ComplianceTemplatesComponent} from './compliance-templates/compliance-templates.component';
-import {ComplianceReportViewerComponent} from "./compliance-report-viewer/compliance-report-viewer.component";
-import { CompliancePrintViewComponent } from './compliance-reports-view/components/compliance-print-view/compliance-print-view.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'templates'},
