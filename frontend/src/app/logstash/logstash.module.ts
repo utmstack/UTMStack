7e4a0c0d9b471a769391392205f50d1b72b55541
Manuel Abascal <mjabascal10@gmail.com>
2026-02-24 15:10:17 -0600

    feat(logstash): integrate Monaco Editor for YAML filter definition and enhance form styling
    Signed-off-by: Manuel Abascal <mjabascal10@gmail.com>

--- frontend/src/app/logstash/logstash.module.ts
+++ frontend/src/app/logstash/logstash.module.ts
@@ -2,29 +2,30 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgSelectModule} from '@ng-select/ng-select';
 import {InlineSVGModule} from 'ng-inline-svg';
+import {MonacoEditorModule} from 'ngx-monaco-editor';
 import {AlertManagementSharedModule} from '../data-management/alert-management/shared/alert-management-shared.module';
 import {UtmSharedModule} from '../shared/utm-shared.module';
 import {LogstashFilterCreateComponent} from './logstash-filters/logstash-filter-create/logstash-filter-create.component';
 import {LogstashFiltersComponent} from './logstash-filters/logstash-filters.component';
 import {LogstashPipelinesComponent} from './logstash-pipelines/logstash-pipelines.component';
 import {LogstashRoutingModule} from './logstash-routing.module';
-import {NgSelectModule} from '@ng-select/ng-select';
-import {DataTypeService} from '../rule-management/services/data-type.service';
 
 @NgModule({
   declarations: [LogstashFiltersComponent, LogstashFilterCreateComponent, LogstashPipelinesComponent],
-    imports: [
-        CommonModule,
-        UtmSharedModule,
-        FormsModule,
-        NgbModule,
-        LogstashRoutingModule,
-        InlineSVGModule,
-        AlertManagementSharedModule,
-        NgSelectModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    CommonModule,
+    UtmSharedModule,
+    FormsModule,
+    NgbModule,
+    LogstashRoutingModule,
+    InlineSVGModule,
+    AlertManagementSharedModule,
+    NgSelectModule,
+    ReactiveFormsModule,
+    MonacoEditorModule
+  ],
   entryComponents: [LogstashFilterCreateComponent]
 })
 export class LogstashModule {
