9995388fd81c1b20fcc1f31a00c0d035ba8a77d1
Manuel Abascal <mjabascal10@gmail.com>
2026-03-02 10:49:53 -0600

    fix: update cisco-meraki correlation rules

---
+++ backend/src/main/resources/config/liquibase/data/20260302/cisco-meraki/utm_group_rules_data_type.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.utm_group_rules_data_type (rule_id, data_type_id, last_update) VALUES (1179, 5, null);
+INSERT INTO public.utm_group_rules_data_type (rule_id, data_type_id, last_update) VALUES (1180, 5, null);
+INSERT INTO public.utm_group_rules_data_type (rule_id, data_type_id, last_update) VALUES (1181, 5, null);
+INSERT INTO public.utm_group_rules_data_type (rule_id, data_type_id, last_update) VALUES (1182, 5, null);
+INSERT INTO public.utm_group_rules_data_type (rule_id, data_type_id, last_update) VALUES (1183, 5, null);
+INSERT INTO public.utm_group_rules_data_type (rule_id, data_type_id, last_update) VALUES (1184, 5, null);
+INSERT INTO public.utm_group_rules_data_type (rule_id, data_type_id, last_update) VALUES (1185, 5, null);
