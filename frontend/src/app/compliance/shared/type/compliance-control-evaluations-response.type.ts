dd6053348a68b42a9b77668239b673c44729f498
Elena Lopez Milan <elopez@utmstack.com>
2026-03-16 18:31:37 -0500

    feat: implement timeline visualization for compliance evaluations

--- frontend/src/app/compliance/shared/type/compliance-control-evaluations-response.type.ts
+++
@@ -1,7 +0,0 @@
-import {ComplianceControlEvaluationsType} from './compliance-control-evaluations.type';
-
-export interface ComplianceControlEvaluationsResponse {
-  startDate: string;
-  endDate: string;
-  evaluations: ComplianceControlEvaluationsType[];
-}
--- frontend/src/app/compliance/shared/type/compliance-index-pattern-query-group-evaluation.type.ts
+++
@@ -1,8 +0,0 @@
-import {ComplianceQueryEvaluationType} from './compliance-query-evaluation.type';
-
-export class ComplianceIndexPatternQueryGroupEvaluationType {
-  indexPatternId?: number;
-  indexPatternName?: string;
-  queries?: ComplianceQueryEvaluationType[];
-}
-
