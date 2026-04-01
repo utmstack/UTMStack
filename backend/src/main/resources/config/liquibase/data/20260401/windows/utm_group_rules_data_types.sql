cca333ef5e8b93080359506d189f47ca5e2809f4
AlexSanchez-bit <sanchez.saez.alex01@gmail.com>
2026-04-01 16:21:39 -0400

    changeset[backend](windows_rules): updated windows rules data types

---
+++ backend/src/main/resources/config/liquibase/data/20260401/windows/utm_group_rules_data_types.sql
@@ -0,0 +1,108 @@
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1356, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1357, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1358, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1359, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1360, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1361, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1362, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1363, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1364, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1365, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1366, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1367, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1368, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1369, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1370, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1371, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1372, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1373, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1374, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1375, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1376, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1377, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1378, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1379, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1380, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1381, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1382, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1383, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1384, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1385, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1386, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1387, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1388, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1389, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1390, 1);
+INSERT INTO public.utm_group_rules_data_type
+(rule_id, data_type_id)
+VALUES(1391, 1);
