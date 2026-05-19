7873f143200bc2f26d27f56d19bb03cdc2efe0e5
Elena Lopez Milan <elopez@utmstack.com>
2026-05-18 19:51:49 -0500

    refactor: remove outdated TODO comment in cp-standard-management.component.ts

--- frontend/src/app/compliance/compliance-management/cp-standard-management/cp-standard-management.component.ts
+++ frontend/src/app/compliance/compliance-management/cp-standard-management/cp-standard-management.component.ts
@@ -51,7 +51,6 @@ export class CpStandardManagementComponent implements OnInit {
     });
   }
 
-  //TODO: ELENA to be checked
   newCompliance() {
     // this.modalService.open(UtmComplianceCreateComponent, {centered: true});
     this.modalService.open(UtmComplianceControlCreateComponent, {centered: true, size: 'lg'});
