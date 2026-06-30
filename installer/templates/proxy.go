e5c381cac9e9a02918a2d3887538685b505c2bca
Alex Sánchez <sanchez.saez.alex01@gmail.com>
2026-06-30 11:42:35 -0600

    fix[installer](proxy): added upgrade header so ws connections can math between frontend and backend

--- installer/templates/proxy.go
+++ installer/templates/proxy.go
@@ -15,6 +15,9 @@ server {
 
     location / {
         proxy_pass  $utmstack;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -69,6 +72,9 @@ server {
 
     location / {
         proxy_pass  $utmstack;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
