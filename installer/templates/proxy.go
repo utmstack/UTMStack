e3b0d527c919a35da29792f9e5134cf512dbed2f
Alex Sánchez <sanchez.saez.alex01@gmail.com>
2026-07-01 07:59:18 -0600

    fix[installer](weboscket): added lookup table to handle upgrade headers properly

--- installer/templates/proxy.go
+++ installer/templates/proxy.go
@@ -1,6 +1,11 @@
 package templates
 
-const ProxyUbuntu string = `server {
+const ProxyUbuntu string = `map $http_upgrade $connection_upgrade {
+    default upgrade;
+    ''      close;
+}
+
+server {
     listen 80 default_server;
     server_name _;
     return 301 https://$host$request_uri;
@@ -17,7 +22,7 @@ server {
         proxy_pass  $utmstack;
         proxy_http_version 1.1;
         proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "Upgrade";
+        proxy_set_header Connection $connection_upgrade;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -57,6 +62,11 @@ http {
 
     keepalive_timeout  65;
 
+    map $http_upgrade $connection_upgrade {
+        default upgrade;
+        ''      close;
+    }
+
 server {
     listen 80 default_server;
     server_name _;
@@ -74,7 +84,7 @@ server {
         proxy_pass  $utmstack;
         proxy_http_version 1.1;
         proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "Upgrade";
+        proxy_set_header Connection $connection_upgrade;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
