3b3e9d8d84ec5dbc0947d4811705b1632b2dabe5
Yadian Llada Lopez <yadian.llada@gmail.com>
2026-07-20 15:28:26 -0400

    refactor(backend/eventprocessing): remove unnecessary internal deactivate endpoint

--- backend/modules/eventprocessing/dto/correlation_rule_internal.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type InternalDeactivateRuleRequest struct {
-	RuleName string `json:"ruleName" binding:"required"`
-}
-
-type InternalDeactivateRuleResponse struct {
-	Changed bool `json:"changed"`
-}
