f0f4d3caa34fe87ffd38a3a2989f7f2c54a72a67
Alex Sánchez <alex.sanchez@utmstack.com>
2026-09-18 14:24:58 -0600

    fix[backend](init):add transactional support to DefinitionSyncService
    Signed-off-by: Alex Sánchez  <alex.sanchez@utmstack.com>

--- backend/src/main/java/com/park/utmstack/service/DefinitionSyncService.java
+++ backend/src/main/java/com/park/utmstack/service/DefinitionSyncService.java
@@ -19,6 +19,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 import org.yaml.snakeyaml.Yaml;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.nio.file.Files;
