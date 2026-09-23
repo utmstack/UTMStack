13122ef698bbb0512ac2bd1dd6d2d8e387bbfadf
Alex Sánchez <sanchez.saez.alex01@gmail.com>
2026-09-23 13:58:46 -0600

    fix[backend](user_service): added admin role in federation user creation

--- backend/src/main/java/com/park/utmstack/service/UserService.java
+++ backend/src/main/java/com/park/utmstack/service/UserService.java
@@ -167,6 +167,8 @@ public class UserService {
 
             if (!Objects.isNull(user.getId())) {
                 user.setPassword(passwordEncoder.encode(password));
+                if (user.getAuthorities().stream().noneMatch(a -> a.getName().equals(AuthoritiesConstants.ADMIN)))
+                    user.getAuthorities().add(authorityRepository.findById(AuthoritiesConstants.ADMIN).orElseThrow());
             } else {
                 user.setLogin(Constants.FS_USER);
                 user.setFirstName("Federation");
@@ -175,7 +177,7 @@ public class UserService {
                 user.setLangKey(Constants.DEFAULT_LANGUAGE);
                 user.setPassword(passwordEncoder.encode(password));
                 user.setActivated(true);
-                Set<Authority> authorities = Stream.of(AuthoritiesConstants.USER).map(authorityRepository::findById).filter(
+                Set<Authority> authorities = Stream.of(AuthoritiesConstants.USER, AuthoritiesConstants.ADMIN).map(authorityRepository::findById).filter(
                     Optional::isPresent).map(Optional::get).collect(Collectors.toSet());
                 user.setAuthorities(authorities);
                 user.setFsManager(true);
